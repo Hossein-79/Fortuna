d7fa6d73d45df36256437c31b523503f81f23ee8
Ali Mousavi <amousavig@icloud.com>
2024-09-14 23:03:55 +0330

    Added save functionality to edit profile.

--- supabase/functions/edit-user/index.ts
+++ supabase/functions/edit-user/index.ts
@@ -1,46 +1,35 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-// Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import { createClient } from 'jsr:@supabase/supabase-js@2'
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/cors.ts";
 
 Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
   try {
-    const supabase = createClient(
-      Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
-    );
+    const supabase = createClient(Deno.env.get("SUPABASE_URL") ?? "", Deno.env.get("SUPABASE_ANON_KEY") ?? "", {
+      global: { headers: { ...corsHeaders, Authorization: req.headers.get("Authorization")! } },
+    });
 
     const { name, bio, profile_picture, email, wallet_address } = await req.json();
 
     const { data, error } = await supabase
-      .from('users')
-      .upsert({ name, bio, profile_picture, email, wallet_address }, { onConflict: ['wallet_address'] });
+      .from("users")
+      .upsert({ name, bio, profile_picture, email, wallet_address }, { onConflict: ["wallet_address"] });
 
     if (error) {
       throw new Error(error.message);
     }
 
-    return new Response(JSON.stringify({
-      message: 'User updated successfully',
-      data,
-    }), { headers: { "Content-Type": "application/json" } });
+    return new Response(
+      JSON.stringify({
+        message: "User updated successfully",
+        data,
+      }),
+      { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+    );
   } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
   }
-})
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/edit-user' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+});
