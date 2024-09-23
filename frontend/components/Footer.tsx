526503667b2a4791b216c1404b69373521a37cd8
Ali Mousavi <amousavig@icloud.com>
2024-09-23 17:07:54 +0330

    Updated footer UI.

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,3 +1,23 @@
+import { CodeIcon, GithubIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="px-5 py-7 flex justify-between items-center text-sm bg-slate-100">
+      <div className="flex items-center">
+        <CodeIcon className="w-4 h-4 mr-2" />
+        Designed and developed by{" "}
+        <Link to="https://github.com/almoloo>" className="text-emerald-500 mx-1">
+          Ali
+        </Link>
+        {" and "}
+        <Link to="https://github.com/hossein-79" className="text-emerald-500 mx-1">
+          Hossein
+        </Link>
+      </div>
+      <Link to="https://github.com/Hossein-79/Fortuna" className="text-neutral-500">
+        <GithubIcon className="w-4 h-4" />
+      </Link>
+    </footer>
+  );
 }
