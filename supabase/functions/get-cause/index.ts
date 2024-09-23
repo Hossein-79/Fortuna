5433624c1fc3067e2ac04db8d23c753835b843f9
Ali Mousavi <amousavig@icloud.com>
2024-09-23 17:19:09 +0330

    Created get cause API.

---
+++ supabase/functions/get-cause/index.ts
@@ -0,0 +1,42 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { createClient } from "jsr:@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/cors.ts";
+
+
+Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    const supabase = createClient(Deno.env.get("SUPABASE_URL") ?? "", Deno.env.get("SUPABASE_ANON_KEY") ?? "", {
+      global: { headers: { ...corsHeaders, Authorization: req.headers.get("Authorization")! } },
+    });
+
+    const url = new URL(req.url);
+    const id = url.searchParams.get("id");
+    if (!id) {
+      throw new Error("id query parameter is required");
+    }
+
+    const { data, error } = await supabase
+      .from("causes")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return new Response(
+      JSON.stringify({
+        data,
+      }),
+      { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+})
+
