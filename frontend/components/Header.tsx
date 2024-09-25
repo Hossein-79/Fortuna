98abd36cedce3b9f8b492b95df47b0e0d3315744
Ali Mousavi <amousavig@icloud.com>
2024-09-25 19:16:09 +0330

    Removed extra junk.

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -15,13 +15,6 @@ export function Header() {
           <WalletSelector />
         </nav>
       </header>
-      <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
-        <h1 className="display">Boilerplate Template</h1>
-
-        <div className="flex gap-2 items-center flex-wrap">
-          <WalletSelector />
-        </div>
-      </div>
     </>
   );
 }
