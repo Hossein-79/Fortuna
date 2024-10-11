e5ab6456e1ad7cec868281911625b5cbf0da57b1
Ali Mousavi <amousavig@icloud.com>
2024-10-12 02:08:52 +0330

    Fixed gutter.

--- frontend/routes/Explore.tsx
+++ frontend/routes/Explore.tsx
@@ -48,7 +48,7 @@ export default function Explore() {
       ) : (
         <>
           {fetchedData && fetchedData?.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-5">
               {fetchedData?.map((cause) => <CauseItem key={cause.id} cause={cause} />)}
             </div>
           ) : (
